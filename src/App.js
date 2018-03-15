aa126e25e1ad88bd83dc03e01ab259810bc3eca3
SuperBob5000 <htaylor3005@gmail.com>
2018-03-15 16:40:02 +0000

    1. Added react-typist package 2. Utilised Typist tag to create title 3. Added footer with logos and links to relevant pages.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,42 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Typist from 'react-typist';
+import githubLogo from './github-icon.svg';
+import linkedinLogo from './iconmonstr-linkedin-5.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Typist
+          cursor={{ show: false }}
+          startDelay={750}
+        >
+          <h1 className="Name">Howell T</h1>
+          <Typist.Backspace count={8} delay={750} />
+          <h1 className="Name">Hiwel Tay</h1>
+          <Typist.Backspace count={9} delay={750} />
+          <h1 className="Name">F@*!?...</h1>
+          <Typist.Backspace count={8} delay={1000} />
+          <h1 className="Name">Hywel Taylor</h1>
+          <h2 className="Job">Software Developer
+          <Typist.Delay ms={500} />...?
+          </h2>
+          <Typist.Backspace count={4} delay={1000} />
+        </Typist>
+
+        <footer>
+          <div class="logos">
+            <a href="https://github.com/SuperBob5000">
+              <img src={githubLogo} alt="Github" class="logos"/>
+            </a>
+          </div>
+          <div class="logos">
+            <a href="https://www.linkedin.com/in/hywel-taylor-505a3690/">
+              <img src={linkedinLogo} alt="Linkedin" class="logos"/>
+            </a>
+          </div>
+        </footer>
       </div>
     );
   }
